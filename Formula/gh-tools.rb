5a9f203c8b9f14bd6770f5affc616b3389ef0c45
Peter Matseykanets <pmatseykanets@gmail.com>
2021-10-17 12:13:54 -0400

    Update gh-tools formula

--- Formula/gh-tools.rb
+++ Formula/gh-tools.rb
@@ -6,22 +6,20 @@ class GhTools < Formula
   bottle :unneeded
 
   on_macos do
-    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+    if Hardware::CPU.arm?
       url "https://github.com/pmatseykanets/gh-tools/releases/download/v0.9.5/gh-tools_0.9.5_darwin_arm64.tar.gz"
       sha256 "5b0d9c2a8706e64f4c0e4f2b87ab0eafced1ffe1c084938dca612ab2e190425e"
-    end
-    if Hardware::CPU.intel?
+    elsif Hardware::CPU.intel?
       url "https://github.com/pmatseykanets/gh-tools/releases/download/v0.9.5/gh-tools_0.9.5_darwin_amd64.tar.gz"
       sha256 "c991d80a66dc30c8c12a4d5ad1194c2d98679d6c0f5f60d13666cdf7617447b9"
     end
   end
 
   on_linux do
-    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+    if Hardware::CPU.arm?
       url "https://github.com/pmatseykanets/gh-tools/releases/download/v0.9.5/gh-tools_0.9.5_linux_arm64.tar.gz"
       sha256 "48e18b652f17ddf8f0532a69db2fb475124222f656baa7dda79c64bb5add09cd"
-    end
-    if Hardware::CPU.intel?
+    elsif Hardware::CPU.intel?
       url "https://github.com/pmatseykanets/gh-tools/releases/download/v0.9.5/gh-tools_0.9.5_linux_amd64.tar.gz"
       sha256 "fb8748360476ed7e4dce9ace60ba275e19dbe0c87ebc8c5f7c86a020c19a951f"
     end
